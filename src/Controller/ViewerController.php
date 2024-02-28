50ad3877b18f2ac0b005ba8fe2de1729bcff1341
Hubert Figuière <hub@collabora.com>
2024-02-28 16:57:46 -0500

    controller: added subbed WOPI end points.
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- src/Controller/ViewerController.php
+++ src/Controller/ViewerController.php
@@ -3,11 +3,73 @@ namespace Drupal\collabora_online\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\collabora_online\Cool\CoolRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Provides route responses for the Collabora module.
  */
 class ViewerController extends ControllerBase {
+
+    function wopiCheckFileInfo(string $id) {
+        // test.txt is just a fake text file
+        // the Size property is the length of the string
+        // returned in wopiGetFile
+        $payload = [
+            'BaseFileName' => 'test.txt',
+            'Size' => 11,
+            'UserId' => 1,
+            'UserCanWrite' => true
+        ];
+
+        $jsonPayload = json_encode($payload);
+
+        $response = new Response(
+            $jsonPayload,
+            Response::HTTP_OK,
+            ['content-type' => 'application/json']
+        );
+        return $response;
+    }
+
+    function wopiGetFile() {
+        $response = new Response(
+            'Hello WOPI ' . $id . ' action ' . $action,
+            Response::HTTP_OK,
+            ['content-type' => 'text/plain']
+        );
+        return $response;
+    }
+
+    function wopiPutFile() {
+        $response = new Response(
+            '<p>WOPI ' . $id . ' action ' . $action . '</p>',
+            Response::HTTP_OK,
+            ['content-type' => 'text/plain']
+        );
+        return $response;
+    }
+
+    public function wopi(string $action, string $id) {
+        $returnCode = Response::HTTP_BAD_REQUEST;
+        switch ($action) {
+        case 'info':
+            return $this->wopiCheckFileInfo($id);
+            break;
+        case 'content':
+            return $this->wopiGetFile($id);
+            break;
+        case 'save':
+            $returnCode = Response::HTTP_OK;
+            break;
+        }
+
+        $response = new Response(
+            '<p>WOPI ' . $id . ' action ' . $action . '</p>',
+            $returnCode,
+            ['content-type' => 'text/plain']
+        );
+        return $response;
+    }
 
     /**
      * Returns a simple page.
