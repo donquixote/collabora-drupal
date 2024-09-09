71c2c4735e0c23ce80f514367cf604c0a88f6191
Andreas Hennings <andreas@dqxtech.net>
2024-09-10 01:20:21 +0200

    Issue #27: Replace global permissions with per-bundle perms and 'edit own'.

---
+++ src/CollaboraMediaPermissions.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\collabora_online;
+
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\BundlePermissionHandlerTrait;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\media\MediaTypeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides permissions for Collabora per media type.
+ *
+ * @see \Drupal\media\MediaPermissions
+ */
+class CollaboraMediaPermissions implements ContainerInjectionInterface {
+
+    use AutowireTrait;
+    use BundlePermissionHandlerTrait;
+    use StringTranslationTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+     *   The entity type manager service.
+     */
+    public function __construct(
+        protected readonly EntityTypeManagerInterface $entityTypeManager,
+    ) {}
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static($container->get('entity_type.manager'));
+    }
+
+    /**
+     * Returns an array of media type permissions.
+     *
+     * @return array
+     *   The media type permissions.
+     *
+     * @see \Drupal\user\PermissionHandlerInterface::getPermissions()
+     */
+    public function mediaTypePermissions(): array {
+        // Generate media permissions for all media types.
+        $media_types = $this->entityTypeManager->getStorage('media_type')->loadMultiple();
+        return $this->generatePermissions($media_types, [$this, 'buildPermissions']);
+    }
+
+    /**
+     * Returns a list of permissions for a given media type.
+     *
+     * @param \Drupal\media\MediaTypeInterface $type
+     *   The media type.
+     *
+     * @return array
+     *   An associative array of permission names and descriptions.
+     */
+    protected function buildPermissions(MediaTypeInterface $type) {
+        $type_id = $type->id();
+        $type_params = ['%type_name' => $type->label()];
+
+        return [
+            "preview $type_id in collabora" => [
+                'title' => $this->t('%type_name: Preview media file in Collabora', $type_params),
+            ],
+            "edit own $type_id in collabora" => [
+                'title' => $this->t('%type_name: Edit own media file in Collabora', $type_params),
+            ],
+            "edit any $type_id in collabora" => [
+                'title' => $this->t('%type_name: Edit any media file in Collabora', $type_params),
+            ],
+        ];
+    }
+
+}
