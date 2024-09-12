15fd45721245f0e9b6803776866cb68863ab9866
Andreas Hennings <andreas@dqxtech.net>
2024-09-12 19:20:40 +0200

    Issue #37: Add missing parentheses.

--- src/Controller/ViewerController.php
+++ src/Controller/ViewerController.php
@@ -74,7 +74,7 @@ class ViewerController extends ControllerBase {
         $render_array = CoolUtils::getViewerRender($media, $edit, $options);
 
         if (!$render_array ||  array_key_exists('error', $render_array)) {
-            $error_msg = 'Viewer error: ' . $render_array ? $render_array['error'] : 'NULL';
+            $error_msg = 'Viewer error: ' . ($render_array ? $render_array['error'] : 'NULL');
             \Drupal::logger('cool')->error($error_msg);
             return new Response(
                 $error_msg,
