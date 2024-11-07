e4e4e73ae842c33763f2c2bee237179bb2c0f5ec
Andreas Hennings <andreas@dqxtech.net>
2024-11-07 13:30:38 +0100

    Issue #49: Replicate PermissionTest as a kernel test.

---
+++ tests/src/Kernel/PermissionTest.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\collabora_online\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
+use Drupal\user\PermissionHandlerInterface;
+
+/**
+ * Tests dynamically created permissions.
+ */
+class PermissionTest extends KernelTestBase {
+
+    use MediaTypeCreationTrait;
+    use UserCreationTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $modules = [
+        'collabora_online',
+        'media',
+        'user',
+        'field',
+        'system',
+        'file',
+        'image',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->installEntitySchema('user');
+        $this->installEntitySchema('file');
+        $this->installSchema('file', 'file_usage');
+        $this->installEntitySchema('media');
+        $this->installConfig(['field', 'system', 'user', 'file', 'media']);
+    }
+
+    /**
+     * Tests that dynamic permissions are properly created.
+     */
+    public function testDynamicPermissions(): void {
+        $this->createMediaType('file', [
+            'id' => 'public_wiki',
+            'label' => 'Public wiki',
+        ]);
+        /** @var \Drupal\user\PermissionHandlerInterface $permission_handler */
+        $permission_handler = \Drupal::service(PermissionHandlerInterface::class);
+        $permissions = $permission_handler->getPermissions();
+        $permissions = array_filter(
+            $permissions,
+            fn (array $permission) => $permission['provider'] === 'collabora_online',
+        );
+        // Remove noise that is hard to diff.
+        $permissions = array_map(
+            static function (array $permission) {
+                $permission['title'] = (string) $permission['title'];
+                if ($permission['description'] === NULL) {
+                    unset($permission['description']);
+                }
+                if ($permission['provider'] === 'collabora_online') {
+                    unset($permission['provider']);
+                }
+                return $permission;
+            },
+            $permissions,
+        );
+        ksort($permissions);
+        $this->assertSame([
+            'administer collabora instance' => [
+                'title' => 'Administer the Collabora instance',
+                'restrict access' => TRUE,
+            ],
+            'edit any public_wiki in collabora' => [
+                'title' => '<em class="placeholder">Public wiki</em>: Edit any media file in Collabora',
+                'dependencies' => ['config' => ['media.type.public_wiki']],
+            ],
+            'edit own public_wiki in collabora' => [
+                'title' => '<em class="placeholder">Public wiki</em>: Edit own media file in Collabora',
+                'dependencies' => ['config' => ['media.type.public_wiki']],
+            ],
+            'preview public_wiki in collabora' => [
+                'title' => '<em class="placeholder">Public wiki</em>: Preview media file in Collabora',
+                'dependencies' => ['config' => ['media.type.public_wiki']],
+            ],
+        ], $permissions);
+    }
+
+}
