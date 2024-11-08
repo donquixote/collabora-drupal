1a2c9283d706526de667e1c2ec557c22208f5e73
AaronGilMartinez <aaron.gil-martinez@ext.ec.europa.eu>
2024-11-08 13:42:41 +0100

    Issue CollaboraOnline/collabora-drupal#52: Add title content check.

--- modules/collabora_online_group/tests/src/Kernel/PermissionTest.php
+++ modules/collabora_online_group/tests/src/Kernel/PermissionTest.php
@@ -32,6 +32,7 @@ class PermissionTest extends GroupKernelTestBase {
         // Generate types: group and media.
         $group_type_1 = $this->createGroupType();
         $group_type_2 = $this->createGroupType();
+        $group_type_3 = $this->createGroupType();
         $media_type_1 = $this->createMediaType('file');
         $media_type_2 = $this->createMediaType('file');
 
@@ -61,20 +62,41 @@ class PermissionTest extends GroupKernelTestBase {
         // Check that permissions are generated for the groups.
         // The 'group_1' has only 'media_type_1' permissions.
         $permissions_group_1 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_1);
-        $this->assertArrayHasKey("preview group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayHasKey("edit any group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayHasKey("edit own group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayNotHasKey("preview group_media:{$media_type_2->id()} in collabora", $permissions_group_1);
-        $this->assertArrayNotHasKey("edit any group_media:{$media_type_2->id()} in collabora", $permissions_group_1);
-        $this->assertArrayNotHasKey("edit own group_media:{$media_type_2->id()} in collabora", $permissions_group_1);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_1);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_1, FALSE);
         // The 'group_2' has 'media_type_1' and 'media_type_2' permissions.
         $permissions_group_2 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_2);
-        $this->assertArrayHasKey("preview group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayHasKey("edit any group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayHasKey("edit own group_media:{$media_type_1->id()} in collabora", $permissions_group_1);
-        $this->assertArrayHasKey("preview group_media:{$media_type_2->id()} in collabora", $permissions_group_2);
-        $this->assertArrayHasKey("edit any group_media:{$media_type_2->id()} in collabora", $permissions_group_2);
-        $this->assertArrayHasKey("edit own group_media:{$media_type_2->id()} in collabora", $permissions_group_2);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_2);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_2);
+        // The 'group_3' doesn't have any permission.
+        $permissions_group_3 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_3);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_3, FALSE);
+        $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_3, FALSE);
     }
 
+    /**
+     * Asserts that collabora permissions are present or not in a given array.
+     *
+     * @param string $id
+     *   The entity ID.
+     * @param array $permissions
+     *   The permission where to perform the checks.
+     * @param bool $enabled
+     *   If the permissions are enabled.
+     */
+    protected function assertCollaboraPermissions(string $id, array $permissions, bool $enabled = TRUE): void {
+        $expected_permissions = [
+            "preview $id in collabora" => "Entity: Preview media item in collabora",
+            "edit any $id in collabora" => "Entity: Edit any media item in collabora",
+            "edit own $id in collabora" => "Entity: Edit own media item in collabora",
+        ];
+
+        foreach($expected_permissions as $name => $description) {
+            if ($enabled === FALSE) {
+                $this->assertArrayNotHasKey($name, $permissions);
+                continue;
+            }
+           $this->assertEquals($description, $permissions[$name]['title']);
+        }
+    }
 }
