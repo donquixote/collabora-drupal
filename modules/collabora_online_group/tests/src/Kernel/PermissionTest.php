f76a51d2fc9471f187ff220cbd34b399726fcd04
AaronGilMartinez <aaron.gil-martinez@ext.ec.europa.eu>
2024-11-08 15:11:05 +0100

    Issue CollaboraOnline/collabora-drupal#52: Permission count check.

--- modules/collabora_online_group/tests/src/Kernel/PermissionTest.php
+++ modules/collabora_online_group/tests/src/Kernel/PermissionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\collabora_online_group\Kernel;
 
+use Drupal\Component\DependencyInjection\Container;
 use Drupal\Tests\group\Kernel\GroupKernelTestBase;
 use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
 
@@ -22,7 +23,6 @@ class PermissionTest extends GroupKernelTestBase {
         'media',
         'group',
         'groupmedia',
-        'collabora_online_group',
     ];
 
     /**
@@ -60,18 +60,30 @@ class PermissionTest extends GroupKernelTestBase {
             ->save();
 
         // Check that permissions are generated for the groups.
+        // Save current permissions count for each group.
+        $permissions_handler = \Drupal::service('group.permissions');
+        $count_group_1 = count($permissions_handler->getPermissionsByGroupType($group_type_1));
+        $count_group_2 = count($permissions_handler->getPermissionsByGroupType($group_type_2));
+        $count_group_3 = count($permissions_handler->getPermissionsByGroupType($group_type_3));
+
+        // Check collabora permissions in each group.
+        $this->enableModules(['collabora_online_group']);
         // The 'group_1' has only 'media_type_1' permissions.
+        drupal_static_reset();
         $permissions_group_1 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_1);
         $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_1);
         $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_1, FALSE);
+        $this->assertCount($count_group_1 + 3, $permissions_group_1);
         // The 'group_2' has 'media_type_1' and 'media_type_2' permissions.
-        $permissions_group_2 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_2);
+        $permissions_group_2 = $permissions_handler->getPermissionsByGroupType($group_type_2);
         $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_2);
         $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_2);
-        // The 'group_3' doesn't have any permission.
-        $permissions_group_3 = \Drupal::service('group.permissions')->getPermissionsByGroupType($group_type_3);
+        $this->assertCount($count_group_2 + 6, $permissions_group_2);
+        // The 'group_3' doesn't have any new permissions.
+        $permissions_group_3 = $permissions_handler->getPermissionsByGroupType($group_type_3);
         $this->assertCollaboraPermissions('group_media:' . $media_type_1->id(), $permissions_group_3, FALSE);
         $this->assertCollaboraPermissions('group_media:' . $media_type_2->id(), $permissions_group_3, FALSE);
+        $this->assertCount($count_group_3, $permissions_group_3);
     }
 
     /**
