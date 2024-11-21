e5045ca797d70cc83ce6bc00c5a19dc30428670d
AaronGilMartinez <aaron.gil-martinez@ext.ec.europa.eu>
2024-11-21 18:31:54 +0100

    Issue 52: Add access control to set preview operation for unpublished entities.

---
+++ modules/collabora_online_group/src/Plugin/Group/RelationHandler/CollaboraAccessControl.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\collabora_online_group\Plugin\Group\RelationHandler;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface;
+use Drupal\group\Plugin\Group\RelationHandler\AccessControlTrait;
+use Drupal\group\Plugin\Group\RelationHandlerDefault\AccessControl;
+
+/**
+ * Provides access control for collabora group.
+ */
+class CollaboraAccessControl extends AccessControl {
+
+  use AccessControlTrait;
+
+  /**
+   * Constructs a new GroupAccessControllProvider.
+   *
+   * @param \Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface $parent
+   *   The default access control.
+   */
+  public function __construct(AccessControlInterface $parent) {
+    $this->parent = $parent;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function entityAccess(EntityInterface $entity, $operation, AccountInterface $account, $return_as_object = FALSE) {
+    // Add support for unpublished vs published for "preview in collabora".
+    $check_published = $operation === 'preview in collabora' && $this->implementsPublishedInterface;
+
+    if ($check_published && !$entity->isPublished()) {
+      $operation .= ' unpublished';
+    }
+
+    return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
+  }
+
+}
